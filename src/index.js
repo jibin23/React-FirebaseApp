11f4be7f4dbdfe336ed38d9e6cdc8db40db0d8eb
jibin23 <joseph.jibin1@gmail.com>
2017-12-01 12:52:59 +0530

    React Router integration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 // Just 1 method react from ReactDom
 import { render } from 'react-dom';
 
+//React Router
+import { BrowserRouter, Match, Miss} from 'react-router';
+
+
+
 // import master stylesheet
 import './css/style.css';
 
 import App from './components/App';
 import StorePicker from './components/StorePicker';
 
+// React Router
+const Root = () =>{
+    return(
+        <BrowserRouter>
+            //when at home -> StorePicker
+            <Match exactly pattern="/" component={StorePicker}>
+            </Match>
+        </BrowserRouter>
+    )
+}
+
+
 render(<App/>, document.querySelector('#main'));
+
+
