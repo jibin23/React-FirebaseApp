0c04fc7f09af2db3c49fd0fd9ebcef3cee37c5ef
jibin23 <joseph.jibin1@gmail.com>
2017-12-04 12:52:13 +0530

    Firebase sync

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyD9Rin2n5KkzZ0gI6JHZTnFUP7ishlAthI",
+    authDomain: "reactwesbosjibz.firebaseapp.com",
+    databaseURL: "https://reactwesbosjibz.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes'
-
+import base from '../base';
 
 class App extends React.Component {
     constructor (){
@@ -19,6 +19,18 @@ class App extends React.Component {
             fishes: {},
             order: {}
         };
+    }
+
+    componentWillMount(){
+        this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+        , {
+            context: this,
+            state: 'fishes'
+        });
+    }
+
+    componentWillUnMount(){
+        base.removeBinding(this.ref);
     }
 
     addFish(fish){
@@ -63,7 +75,7 @@ class App extends React.Component {
                         }
                     </ul>
                 </div>
-                <Order/>
+                <Order fishes={this.state.fishes} order={this.state.order}/>
                 <Inventory loadSamples={this.loadSamples} addFish={this.addFish}/>
 
             </div>
